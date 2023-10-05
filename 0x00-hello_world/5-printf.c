6fb146b7c44904a9307e185df787db7a2b3bcdd5
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-05 19:43:43 +0100

    recent c programming push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* giving an outpu with the printf function */
-int main()
+/**
+  * main - Entry point
+  * Description: 'A c programme printing an exact sentence'
+  * Return: When it's 0 it is is a success
+  */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
