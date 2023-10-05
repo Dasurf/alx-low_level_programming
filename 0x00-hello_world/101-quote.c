a319a7723213cbf66e1c2677505955048940fa33
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-06 00:23:20 +0100

    recent c programming push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: output a function but this time we return an error
+ *
+ * Return: exit code 1 indicating an error.
+ */
+
+int main(void)
+{
+	char quo[] = "and piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}
