2e4d3d3371cec26620ccf8de4666a2ff80fc7e1c
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-05 19:25:53 +0100

    recent c programming push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* 
+* Description: 'A c programme printing an exact sentence'
+* 
+* Return: When it's 0 it is is a success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
