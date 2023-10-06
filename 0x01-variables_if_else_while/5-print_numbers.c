5b18b5312e0b236efe85c7f43a8ea746d7e9ad3c
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-06 18:51:32 +0100

    recent c programming push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'A programme that prints all lower case letters except q and e'
+ * Return: When it's not 0 then it is a success
+ */
+
+int main(void)
+{
+	unsigned int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
