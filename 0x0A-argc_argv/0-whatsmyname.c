cc338ae8862748d3cc4afbfdb00dc0294df3e63f
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-27 16:58:02 +0100

    C Programming latest push

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This is a program that prints its own name
+ *
+ * @argc: This holds the number of arguments passed
+ * @argv: This is an array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(int argc, char* argv[])
 {
