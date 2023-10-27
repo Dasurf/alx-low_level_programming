7af21549694288994e97c109595aa7088e0ab2e9
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-28 00:38:32 +0100

    C Programming latest push

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+
+	return (0);
+}
