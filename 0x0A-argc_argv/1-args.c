26efe9502582ed52892dfec00f6450906bb4bdc5
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-27 17:17:04 +0100

    C Programming latest push

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - This is a program that prints the number of arguments
+ *        that has been passed into it
+ *
+ * @argc: This holds the number of arguments passed
+ * @argv: This is an array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
