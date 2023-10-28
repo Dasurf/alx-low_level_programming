399b82ca150540f4cc61ea5310ab0a76ed103b90
Dasurf <damilareyusuf.dy@gmail.com>
2023-10-28 04:53:39 +0100

    C Programming latest push

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A script that creates a static library
-# called liball.a from all the .c 
+# called liball.a from all the .c
 # files that are in the current directory
 
 gcc -c *.c
