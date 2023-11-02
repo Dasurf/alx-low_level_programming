55a1a2db93488f1fa6dee740bd49662cef9cb3e5
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-02 19:16:51 +0100

    C Programming latest push

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * @malloc_checked: function that allocates memory space
+ * 					for a programme
+ * 
+ * @b: size of the array
+ * @array: the array created
+ * 
+ * Return: returns address in memory of the array created
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	void *array;
+
+	if (b == 0)
+	{
+		return (NULL);
+	}
 
 	array = malloc(b);
 
