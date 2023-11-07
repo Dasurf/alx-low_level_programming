843fa357bb7ff3d2701f89bd3c4f0e130e9090fb
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-08 00:29:13 +0100

    C Programming latest push

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - a structure to get dog details
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ * @d: the dog structure
+ *
+ * return - returns structure of dog details
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name =  name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
