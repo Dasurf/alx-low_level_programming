affffec1c64de244837586b7689a742f2e963ace
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-07 23:38:44 +0100

    C Programming latest push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdlib.h>
+
+/**
+ * struct dog - a structure that holds a dog's name
+ *          age and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+*/
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
