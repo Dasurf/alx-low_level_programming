a90559db243f0bc90cc5dbe04596153a7b4afd0b
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-08 19:29:28 +0100

    C Programming latest push

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/**
+ * print_name - a function that prints a name
+ *
+ * @name: the name to be printed
+ * @f: a function pointer to a string
+ *
+ * return - return nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
