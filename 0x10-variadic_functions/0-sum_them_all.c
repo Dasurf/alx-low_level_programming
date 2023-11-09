04a8025c2db1368cf86b1fd6437d8ee7e7353c2c
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-09 15:32:06 +0100

    C Programming latest push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,15 +19,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	if (n == 0)
-	{
 		return (sum);
-	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(args, int);
-		}
+		sum += va_arg(args, int);
 	}
 	return (sum);
 }
