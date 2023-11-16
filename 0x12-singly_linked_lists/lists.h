cb3e02e1788b5da48fac08f9d9060fe7650baa21
Dasurf <damilareyusuf.dy@gmail.com>
2023-11-16 23:44:50 +0100

    C Programming latest push

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,18 +14,18 @@
  * Description: singly linked list node structure
  * for Holberton project
 */
-
-size_t list_len(const list_t *h);
-void free_list(list_t *head);
-size_t print_list(const list_t *h);
-list_t *add_node_end(list_t **head, const char *str);
-list_t *add_node(list_t **head, const char *str);
-
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LIST_H */
